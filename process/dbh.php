98238f64164fffe2a6f06b28868577438ee6c49c
gijigeorge77 <gijigeorge77@gmail.com>
2022-01-29 10:04:54 +0300

    updated database

--- process/dbh.php
+++ process/dbh.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dbPassword = "";
-$dBName = "Employee";
+$servername = "us-cdbr-east-05.cleardb.net";
+$dBUsername = "b3f410f2854486";
+$dbPassword = "33e32c06";
+$dBName = "heroku_6a79d6ef5482fee";
 
 $conn = mysqli_connect($servername, $dBUsername, $dbPassword, $dBName);
 
